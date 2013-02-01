a4d5fa3c4a69a44f71df316dc8195b1f2daf2266
wkstudio <kolesoxxx10@mail.ru>
2013-02-01 17:57:29 +0200

    http://wcstudio.clockingit.com/tasks/edit/3186619 вид для списка документации, роутинг для редактирования

--- Symfony/src/Start/StartBundle/Controller/StartController.php
+++ Symfony/src/Start/StartBundle/Controller/StartController.php
@@ -9,15 +9,8 @@ class StartController extends Controller
 {
     public function indexAction()
     {
-        /*
-         * The action's view can be rendered using render() method
-         * or @Template annotation as demonstrated in DemoController.
-         *
-         */
-                 //echo "!!!!!!!!!!!!!!!";
         $user = $this->getUser();
         $name = $user->getUsername();   
-        //print_r(get_class_methods ($user));
         return $this->render('StartStartBundle:Start:index.html.twig', array('name' => $name));
 
     }
